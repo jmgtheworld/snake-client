b6c3a1b1a70a249d5b30ec66589f5d104e98db73
vagrant <avedj94@hotmail.com>
2020-11-04 19:26:54 +0000

    wasd movement

--- input.js
+++ input.js
@@ -1,20 +1,51 @@
+// Stores the active TCP connection object.
+let connection; 
+
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  handleUserInput();
+  handleUserInput(conn);
   return stdin;
 }
 
-const handleUserInput = function() {
+const handleUserInput = function(conn) {
   const stdin = process.stdin;
+  connection = conn;
+
   stdin.on('data', (data) => {
     if (data === '\u0003') process.exit();
+  });
+
+  stdin.on('data', (data) => {
+    if (data === 'w') {
+      conn.write("Move: up");
+    }
+  });
+
+  stdin.on('data', (data) => {
+    if (data === 's') {
+      conn.write("Move: down");
+    }
+  });
+
+  stdin.on('data', (data) => {
+    if (data === 'a') {
+      conn.write("Move: left");
+    }
+  });
+
+  stdin.on('data', (data) => {
+    if (data === 'd') {
+      conn.write("Move: right");
+    }
   });
 }
 
