b4e0a970d5269fa1441ba9756b2795a8b9291e2a
vagrant <avedj94@hotmail.com>
2020-11-04 18:30:12 +0000

    added handle userinput and setup user interface

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
-const net = require('net');
 const { connect } = require('./client');
 console.log('Connecting ...');
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+}
+
+const handleUserInput = function() {
+  stdin.on('data', (data) => {
+    if (data === '\u0003') process.exit();
+  });
+}
+
 connect();
