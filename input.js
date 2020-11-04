15ed96d986a3ec7c2cddc7db4c2482d8a9ea5534
vagrant <avedj94@hotmail.com>
2020-11-04 19:30:40 +0000

    refactored a bit of if statements

--- input.js
+++ input.js
@@ -22,31 +22,20 @@ const handleUserInput = function(conn) {
 
   stdin.on('data', (data) => {
     if (data === '\u0003') process.exit();
-  });
-
-  stdin.on('data', (data) => {
     if (data === 'w') {
       conn.write("Move: up");
     }
-  });
-
-  stdin.on('data', (data) => {
     if (data === 's') {
       conn.write("Move: down");
     }
-  });
-
-  stdin.on('data', (data) => {
     if (data === 'a') {
       conn.write("Move: left");
     }
-  });
-
-  stdin.on('data', (data) => {
     if (data === 'd') {
       conn.write("Move: right");
     }
   });
+
 }
 
 module.exports = {setupInput};
