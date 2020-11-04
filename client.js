9f5498690b56218b9cd11f0b580316551e639498
vagrant <avedj94@hotmail.com>
2020-11-04 15:02:41 +0000

    connection event handler and sending a name

--- client.js
+++ client.js
@@ -10,9 +10,21 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+
   conn.on('data', (data) => {
     console.log("you ded cuz you idled");
-  })
+  });
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on('connect', () => {
+    conn.write("Name: MGJ");
+  });
+  
+  conn.setEncoding('utf8'); // interpret data as text
+  
 
   return conn;
 }
